f83e0b72ff977719425136ea2950f14aa6c87e96
zan-rosenthal <zan.rosenthal@gmail.com>
2018-05-03 14:54:53 -0500

    Migrate hoc/portal to use React.createPortal

--- components/hoc/Portal.js
+++ components/hoc/Portal.js
@@ -1,102 +1,34 @@
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
 class Portal extends Component {
   static propTypes = {
-    children: PropTypes.any,
-    container: PropTypes.any,
-    lockBody: PropTypes.bool
+    children: PropTypes.node,
+    className: PropTypes.string,
+    container: PropTypes.node
   }
 
-  static defaultProps = {
-    lockBody: true
+  constructor (props) {
+    super(props);
+    this._portalContainerNode = document.createElement('div');
   }
 
   componentDidMount () {
-    this._renderOverlay();
-  }
-
-  componentWillReceiveProps (nextProps) {
-    if (this._overlayTarget && nextProps.container !== this.props.container) {
-      this._portalContainerNode.removeChild(this._overlayTarget);
-      this._portalContainerNode = getContainer(nextProps.container);
-      this._portalContainerNode.appendChild(this._overlayTarget);
-    }
-  }
-
-  componentDidUpdate () {
-    this._renderOverlay();
+    getContainer(this.props.container).appendChild(this._portalContainerNode);
   }
 
   componentWillUnmount () {
-    this._unrenderOverlay();
-    this._unmountOverlayTarget();
-  }
-
-  _mountOverlayTarget () {
-    if (!this._overlayTarget) {
-      this._overlayTarget = document.createElement('div');
-      this._portalContainerNode = getContainer(this.props.container);
-      this._portalContainerNode.appendChild(this._overlayTarget);
-    }
-  }
-
-  _unmountOverlayTarget () {
-    if (this._overlayTarget) {
-      this._portalContainerNode.removeChild(this._overlayTarget);
-      this._overlayTarget = null;
-    }
-    this._portalContainerNode = null;
-  }
-
-  _renderOverlay () {
-    const overlay = !this.props.children
-      ? null
-      : React.Children.only(this.props.children);
-
-    if (overlay !== null) {
-      this._mountOverlayTarget();
-      this._overlayInstance = ReactDOM.unstable_renderSubtreeIntoContainer(
-        this, overlay, this._overlayTarget
-      );
-    } else {
-      this._unrenderOverlay();
-      this._unmountOverlayTarget();
-    }
-  }
-
-  _unrenderOverlay () {
-    if (this._overlayTarget) {
-      ReactDOM.unmountComponentAtNode(this._overlayTarget);
-      this._overlayInstance = null;
-    }
-  }
-
-  getMountNode () {
-    return this._overlayTarget;
-  }
-
-  getOverlayDOMNode () {
-    if (!this.isMounted()) {
-      throw new Error('getOverlayDOMNode(): A component must be mounted to have a DOM node.');
-    }
-
-    if (this._overlayInstance) {
-      if (this._overlayInstance.getWrappedDOMNode) {
-        return this._overlayInstance.getWrappedDOMNode();
-      } else {
-        return ReactDOM.findDOMNode(this._overlayInstance);
-      }
-    }
-
-    return null;
+    getContainer(this.props.container).removeChild(this._portalContainerNode);
   }
 
   render () {
-    return null;
-  }
+    const portalRender = (
+      <div className={this.props.className}>{this.props.children}</div>
+    );
 
+    return ReactDOM.createPortal(portalRender, this._portalContainerNode);
+  }
 }
 
 function getContainer (container) {
